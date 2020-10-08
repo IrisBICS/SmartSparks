413c8331135b9cce567ee4adb813c451a52ba2b0
Iris Kremer <iris.kremer.001@student.uni.lu>
2020-10-08 13:49:38 +0200

    Minor fixes & updates

--- lib/screens/authenticate/login.dart
+++ lib/screens/authenticate/login.dart
@@ -40,63 +40,65 @@ class _LoginState extends State<Login> {
           )
         ]
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 600,
-            child: Stack(
-              children: <Widget>[
-                Positioned(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(20, 320, 20, 0),
-                    child: SvgPicture.asset('assets/images/undraw_working_late_pukg.svg')
-                    )
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(40, 35, 40, 0),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(bottom: 35),
-                        child: Center(
-                          child: Text(
-                            "Login",
-                            style: TextStyle(color: white, fontSize: 40, fontWeight: FontWeight.bold)
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 600,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(20, 320, 20, 0),
+                      child: SvgPicture.asset('assets/images/undraw_working_late_pukg.svg')
+                      )
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(40, 35, 40, 0),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(bottom: 35),
+                          child: Center(
+                            child: Text(
+                              "Login",
+                              style: TextStyle(color: white, fontSize: 40, fontWeight: FontWeight.bold)
+                            )
+                          )
+                        ),
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                                onChanged: (val) {},
+                              ),
+                              SizedBox(height: 15,),
+                              TextFormField(
+                                decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                                onChanged: (val) {},
+                              ),
+                              SizedBox(height: 30,),
+                              RaisedButton(
+                                color: yellow,
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(color: white, fontSize: 18),
+                                ),
+                                onPressed: () {},
+                              )
+                            ]
                           )
                         )
-                      ),
-                      Form(
-                        key: _formKey,
-                        child: Column(
-                          children: <Widget>[
-                            TextFormField(
-                              decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                              onChanged: (val) {},
-                            ),
-                            SizedBox(height: 15,),
-                            TextFormField(
-                              decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                              onChanged: (val) {},
-                            ),
-                            SizedBox(height: 30,),
-                            RaisedButton(
-                              color: yellow,
-                              child: Text(
-                                'Login',
-                                style: TextStyle(color: white, fontSize: 18),
-                              ),
-                              onPressed: () {},
-                            )
-                          ]
-                        )
-                      )
-                    ]
+                      ]
+                    )
                   )
-                )
-              ]
+                ]
+              )
             )
-          )
-        ]
+          ]
+        ),
       )
     );
   }
